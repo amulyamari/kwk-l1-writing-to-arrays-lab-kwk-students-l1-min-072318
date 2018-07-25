527db74e56f3486e4b465ca0c0f3fd23e1c4e3bb
amulyamari <amulya@saimail.com>
2018-07-25 15:36:17 +0000

    automatically backed up by learn

--- arrays.rb
+++ arrays.rb
@@ -1,10 +1,7 @@
 def rainbow
   change_rainbow_colors=["yellow", "default", "light_cyan"]
-  change_rainbow_colors[0]=
-  change_rainbow_colors[1]
-  change_rainbow_colors[2]
-  
+  change_rainbow_colors[0]="light_yellow"
+  puts change_rainbow_colors
 end
-
 rainbow
  
