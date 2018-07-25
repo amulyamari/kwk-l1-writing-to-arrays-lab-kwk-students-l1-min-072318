242d17f25dfaf16655faab5f0bb0502f17dc0280
amulyamari <amulya@saimail.com>
2018-07-25 15:41:01 +0000

    automatically backed up by learn

--- arrays.rb
+++ arrays.rb
@@ -3,6 +3,8 @@ def rainbow_arrays
   change_rainbow_colors[0]="light_yellow"
   change_rainbow_colors[1]="light_red"
   change_rainbow_colors[2]="red"
+  change_rainbow_colors.push ("green")
+  change_rainbow_colors.push("blue")
   puts change_rainbow_colors
 end
 rainbow_arrays
